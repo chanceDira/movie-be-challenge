71ff50b07e7b6df4c6053a8a7f118c64b30039c6
chanceDira <chancedesire60@gmail.com>
2024-01-15 00:36:10 +0200

    bug: fixed cors issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const movieRoutes = require('./routes/movieRoutes');
+const cors = require('cors'); 
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = require('./swaggerOptions');
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors()); 
 
 app.use('/api', movieRoutes);
 
