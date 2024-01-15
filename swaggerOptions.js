d19cf4475f7abce8724cfd67ed1504c5e7c0a8e0
chanceDira <chancedesire60@gmail.com>
2024-01-15 22:44:42 +0200

    bug: fix doc not showing

--- swaggerOptions.js
+++ swaggerOptions.js
@@ -1,23 +1,46 @@
 const swaggerOptions = {
-    swaggerDefinition: {
+    // swaggerDefinition: {
+    //     info: {
+    //         title: 'Movie API',
+    //         version: '1.0.0',
+    //         description: 'API documentation for the Movie API',
+    //     },
+    //     basePath: '/api',
+    // },
+    // apis: ['./routes/*.js', './controllers/*.js'],
+    // definitions: {
+    //     Movie: {
+    //         type: 'object',
+    //         properties: {
+    //             title: { type: 'string', example: 'Titanic part 5' },
+    //             year: { type: 'number', example: 2010 },
+    //             image: { type: 'string', example: 'image url' },
+    //         },
+    //     },
+    // },
+
+    definition: {
+        openapi: "3.0.0",
         info: {
-            title: 'Movie API',
-            version: '1.0.0',
-            description: 'API documentation for the Movie API',
+          title: "Library API",
+          version: "1.0.0",
+          description: "A simple Express Library API",
+          termsOfService: "http://example.com/terms/",
+          contact: {
+            name: "API Support",
+            url: "http://www.exmaple.com/support",
+            email: "support@example.com",
+          },
         },
-        basePath: '/api',
-    },
-    apis: ['./routes/*.js', './controllers/*.js'],
-    definitions: {
-        Movie: {
-            type: 'object',
-            properties: {
-                title: { type: 'string', example: 'Titanic part 5' },
-                year: { type: 'number', example: 2010 },
-                image: { type: 'string', example: 'image url' },
-            },
-        },
-    },
+        servers: [
+          {
+            url: "https://nodejs-swagger-api.vercel.app/",
+            description: "My API Documentation",
+          },
+        ],
+      },
+      // This is to call all the file
+      apis: ['./routes/*.js', './controllers/*.js'],
 };
 
 module.exports = swaggerOptions;
